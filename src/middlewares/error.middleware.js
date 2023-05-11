1f8d67ce4dbc4690456cdab962ad3a2fda2648ca
Victor Manuel Torrico Diaz <manu.1605.98@gmail.com>
2023-05-11 13:12:13 -0300

    actualizacion

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,6 @@
+export default errorHandler = (err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ error: 'Ocurrió un error en el servidor' });
+  }
+  
+  app.use(errorHandler);
